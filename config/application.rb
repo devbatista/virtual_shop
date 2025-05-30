5ba845bda00e0f87d22a73a5a0500d57e3e43750
Rafael Batista <rafael@devbatista.com>
2025-05-30 15:34:45 -0300

    alterando testes para rodar com rspec

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,15 @@ module VirtualShop
     
     config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
+
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
   end
 end
