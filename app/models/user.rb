a9cd2911d287842b8a3baa716b7fd58e8a107b9b
Rafael Batista <rafael@devbatista.com>
2025-05-30 15:14:11 -0300

    adicionando a estrutura inicial da model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :carts
+  has_many :orders
+
+  has_secure_password
+
+  enum role: { customer: 0, admin: 1 }
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
