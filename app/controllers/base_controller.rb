326b2af010e1ab9ff191dc34a6b28c82fee10a8d
Rafael Batista <rafael@devbatista.com>
2025-05-30 19:37:30 -0300

    criando CRUD de Products com restrição a admins

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,3 +1,9 @@
 class BaseController < ApplicationController
+  private
 
+    def authorize_admin!
+      unless current_user&.admin?
+        redirect_to root_path, alert: "Access restricted to administrators."
+      end
+    end
 end
