c0efce12fe1278f99f05e78358f8758425643eb1
Rafael Batista <rafael@devbatista.com>
2025-05-30 15:25:14 -0300

    order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,15 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :order_item, dependent: :destroy
+
+  enum status: {
+    pending: "pending",
+    paid: "paid",
+    shipped: "shipped",
+    delivered: "delivered",
+    cancelled: "cancelled"
+  }
+
+  validates :status, presence: true
+  validates :total, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
