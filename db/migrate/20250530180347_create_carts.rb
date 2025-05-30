8bdb4fd2da2fabbfe915b30356a02d729f0d8101
Rafael Batista <rafael@devbatista.com>
2025-05-30 15:04:13 -0300

    criando a model Cart

---
+++ db/migrate/20250530180347_create_carts.rb
@@ -0,0 +1,10 @@
+class CreateCarts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :carts, id: :uuid do |t|
+      t.references :user, null: false, foreign_key: true, type: :uuid
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_30_180103) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_30_180347) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "carts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "products", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name"
@@ -33,4 +41,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_30_180103) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "carts", "users"
 end
