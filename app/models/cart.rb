c974150d983f4cb7194c6784e17e49b05a6cb52c
Rafael Batista <rafael@devbatista.com>
2025-05-30 17:59:41 -0300

    adicionando regra de adicionar produto no carrinho

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -10,4 +10,13 @@ class Cart < ApplicationRecord
   }
 
   validates :status, presence: true
+
+  def add_product(product, quantity = 1)
+    return { success: false, errors: "Insufficient stock" } if product.stock < quantity
+
+    cart_item = cart_items.find_or_initialize_by(product: product)
+    cart_item.quantity ||= 0
+    cart_item.quantity += quantity
+    cart_item.save
+  end
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,4 +3,23 @@ class CartItem < ApplicationRecord
   belongs_to :product
 
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0}
+
+  before_save :decrement_product_stock
+
+  private
+
+    def decrement_product_stock
+      if will_save_change_to_quantity?
+        old_quantity, new_quantity = quantity_change_to_be_saved
+        diff = (new_quantity || 0) - (old_quantity || 0)
+        
+        if product.stock >= diff
+          product.stock -= diff
+          product.save!
+        else
+          errors.add(:quantity, "is greater than available stock")
+          throw(:abort)
+        end
+      end
+    end
 end
