1604e2406acdd4b3c53a19be1bdca7587ed16031
Rafael Batista <rafael@devbatista.com>
2025-05-30 15:19:05 -0300

    model Cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,13 @@
 class Cart < ApplicationRecord
   belongs_to :user
+  has_many :cart_items, dependent: :destroy
+  
+  enum status: {
+    active: "active",
+    abandoned: "abandoned",
+    ordered: "ordered",
+    cancelled: "cancelled"
+  }
+
+  validates :status, presence: true
 end
