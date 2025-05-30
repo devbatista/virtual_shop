c0f559186758ec6958092d0597f7ff2341d5c5a4
Rafael Batista <rafael@devbatista.com>
2025-05-30 15:44:59 -0300

    adicionando teste da model user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,11 @@
-require "test_helper"
+require "rails_helper"
 
-class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+RSpec.describe User, type: :model do
+  it { should have_many(:carts) }
+  it { should have_many(:orders) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should define_enum_for(:role).with_values(%i[customer admin]) }
 end
