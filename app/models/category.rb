de2fee41d64ff37ee774ecedbbc2fe9f736a3783
Rafael Batista <rafael@devbatista.com>
2025-06-03 17:32:45 -0300

    ajuste de relacionamento entre categoria e produtos

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 class Category < ApplicationRecord
   belongs_to :parent, class_name: "Category", optional: true, foreign_key: "parent_id"
+  
   has_many :subcategories, class_name: "Category", foreign_key: "parent_id", dependent: :destroy
+  has_many :products, dependent: :nullify
 
   validates :name, presence: true
   validates :name, uniqueness: { scope: :parent_id, message: "must be unique per parent category" }
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,10 @@ class Product < ApplicationRecord
   has_many :cart_items
   has_many :order_items
 
+  belongs_to :category
+
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :stock, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :category, presence: true
 end
