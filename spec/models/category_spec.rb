dab4cd277a668a3c2dd686a223f7900b1a46353e
Rafael Batista <rafael@devbatista.com>
2025-06-03 16:42:51 -0300

    criando escopo de testes de Category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should belong_to(:parent).class_name('Category').optional }
+    it { should have_many(:subcategories).class_name('Category').with_foreign_key('parent_id').dependent(:destroy) }
+  end
+
+  describe "validations" do
+    subject { Category.new(name: "Test") }
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name).scoped_to(:parent_id).with_message("must be unique per parent category") } 
+  end
 end
