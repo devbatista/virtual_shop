b74aeb0d4f2376944929acdf1158b2e92c75e56b
Rafael Batista <rafael@devbatista.com>
2025-06-04 10:56:31 -0300

    criando seed de usuários

---
+++ db/seeds/1-users.rb
@@ -0,0 +1,19 @@
+puts "Creating users"
+
+# create admin user
+User.create!(
+  name: "Rafael Batista",
+  email: "rafael@devbatista.com",
+  password: "senha123",
+  passowd_confirmation: "senha_123",
+  role: :admin
+)
+
+# create customer user
+User.create!(
+  name: "Robertson Costa",
+  email: "robertson@virtualshop.com",
+  password: "senha123",
+  password_confirmation: "senha123",
+  role: :customer
+)
