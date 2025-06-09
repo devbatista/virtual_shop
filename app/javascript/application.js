1ae44083454c82c9156915e8b2746f912ffd28e2
Rafael Batista <rafael@devbatista.com>
2025-06-09 15:18:52 -0300

    testando método delete

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,23 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("turbo:load", () => {
+  document.querySelectorAll("a[data-method='delete']").forEach(link => {
+    link.addEventListener("click", function(event) {
+      event.preventDefault();
+      if (confirm(link.dataset.confirm || "Are you sure?")) {
+        fetch(link.href, {
+          method: "DELETE",
+          headers: {
+            "X-CSRF-Token": document.querySelector("meta[name='csrf-token']").content,
+            "Accept": "text/vnd.turbo-stream.html, text/html, application/xhtml+xml"
+          },
+          credentials: "same-origin"
+        }).then(() => {
+          window.location.href = "/";
+        });
+      }
+    });
+  });
+});
