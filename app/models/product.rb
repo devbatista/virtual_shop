efa6d6902fdd4f81cc7fb2147dbbffe6ed631e60
Rafael Batista <rafael@devbatista.com>
2025-06-12 10:45:24 -0300

    adicionando os produtos na view

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,8 @@ class Product < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :stock, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :category, presence: true
+
+  def in_stock?
+    stock.positive?
+  end
 end
