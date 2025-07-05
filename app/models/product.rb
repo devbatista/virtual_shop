b4f008275350dd44e6806c6d7ee2951388ed76e5
Rafael Batista <rafael@devbatista.com>
2025-07-05 17:58:39 -0300

    criando relacionamento de storage pra produto

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ApplicationRecord
 
   belongs_to :category
   belongs_to :color
+  belongs_to :storage
 
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
