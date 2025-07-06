b99eaa73b61d046266ed13a66f892ef83612e639
Rafael Batista <rafael@devbatista.com>
2025-07-06 12:28:15 -0300

    criando seed para storage

--- db/seeds/0-prepare.rb
+++ db/seeds/0-prepare.rb
@@ -5,5 +5,6 @@ Order.delete_all
 Product.delete_all
 Category.delete_all
 User.delete_all
+Storage.delete_all
 
 puts "Clean environment"
---
+++ db/seeds/5-storage.rb
@@ -0,0 +1,15 @@
+puts "Creating storage"
+
+Storate.create!(
+  name: "256GB"
+)
+
+Storate.create!(
+  name: "512GB"
+)
+
+Storate.create!(
+  name: "1TB"
+)
+
+puts "Storages created successfully"
