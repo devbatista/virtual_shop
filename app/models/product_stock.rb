0d4e4d051e92d5504a7f12bbfd3bc52ba60a57e6
Rafael Batista <rafael@devbatista.com>
2025-07-08 16:57:42 -0300

    adicionando algumas regras para ProductStock

--- app/models/product_stock.rb
+++ app/models/product_stock.rb
@@ -2,4 +2,12 @@ class ProductStock < ApplicationRecord
   belongs_to :product
   belongs_to :color, optional: true
   belongs_to :storage, optional: true
+
+  validates :quantity, numericality: { greater_than_or_equal_to: 0 }
+  validates :color_id, 
+            uniqueness: { 
+              scope: [:product_id, :storage_id],
+              message: 'already exists for this product and storage' 
+            }
+  validates :product, presence: true
 end
